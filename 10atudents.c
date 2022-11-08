754cf862143fb815c5a6e02025bc4627c4644a27
Tobechukwu Chris-Odeh <108990991+Tobeodeh@users.noreply.github.com>
2022-11-08 16:35:35 +0100

    Fix: bugs for omitted semi-colon

--- 10atudents.c
+++ 10atudents.c
@@ -1,20 +1,20 @@
 #include<stdio.h>
 #define Length 10
 #define Line_length 50
-int main(void)
+int main()
 {
 	char names[Line_length][Length];
 	for (int i = 0; i < Length; i++)
 	{
-	   printf("ENTER YOUR NAME: [%s]", i)
-	   gets(names[i])
+	   printf("ENTER YOUR NAME: ", names[i]);
+	   gets(names[i]);
 	   
 	}
 	
 	//displaying result
 	for (int i = 0; i < Length; i++)
 	{
-	    printf("\n %s",names[i]);
+	    printf("\n%s",names[i]);
 	}
         return 0;
 }
